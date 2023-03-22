60536af8014b7e955dc32d2c9ca57ea633ef0063
Dmitri Popov <dmpop@linux.com>
2023-03-22 13:40:41 +0100

    Switch to lit.css

--- edit.php
+++ edit.php
@@ -5,7 +5,7 @@ include('config.php');
 <!DOCTYPE html>
 <html lang="en" data-theme="<?php echo $theme; ?>">
 
-<!-- Author: Dmitri Popov, dmpop@linux.com
+<!-- Author: Dmitri Popov, dmpop@cameracode.coffee
 	 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 
 <head>
@@ -13,8 +13,7 @@ include('config.php');
 	<meta charset="utf-8">
 	<link rel="shortcut icon" href="favicon.png" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/classless.css" />
-	<link rel="stylesheet" href="css/themes.css" />
+	<link rel="stylesheet" href="lit.css" />
 	<!-- Suppress form re-submit prompt on refresh -->
 	<script>
 		if (window.history.replaceState) {
@@ -24,62 +23,60 @@ include('config.php');
 </head>
 
 <body>
-	<div class="card text-center">
-		<div style="margin-top: 1em; margin-bottom: 1em;">
-			<img style="display: inline; height: 2.5em; vertical-align: middle;" src="favicon.svg" alt="logo" />
-			<h1 style="display: inline; margin-top: 0em; vertical-align: middle; letter-spacing: 3px; color: #ffcc02;"><?php echo $title; ?></h1>
-		</div>
-		<hr>
-		<button title="Back" style="margin-top: 1.5em; margin-bottom: 1.5em;" onclick='window.location.href = "index.php"'><img style='vertical-align: middle;' src='svg/back.svg' /></button>
-		<form class="text-left" action=" " method="POST">
-			<label>Station:
-				<input type="text" name="station">
-			</label>
-			<label>Stream URL:
-				<input type="text" name="url">
-			</label>
-			<label>Logo URL:
-				<input type="text" name="logo">
-			</label>
-			<div>
-				<label>Station to remove:
-					<select name="rmstation">
-						<option value="--" selected>Select station</option>
-						<?php
-						$files = glob($dir . DIRECTORY_SEPARATOR . "*");
-						foreach ($files as $file) {
-							echo "<option value='" . $file . "'>" . pathinfo($file)['filename'] . "</option>";
-						}
-						?>
-					</select>
-				</label>
+	<div class="c">
+		<div style="text-align: center;">
+			<div class="card w-100">
+				<img style="display: inline; height: 2.5em; vertical-align: middle;" src="favicon.svg" alt="logo" />
+				<h1 style="display: inline; margin-top: 0em; vertical-align: middle; letter-spacing: 3px; color: #ce6a85;"><?php echo $title; ?></h1>
+				<form style="margin-top: 2em;" action=" " method="POST">
+					<label>Station
+						<input class="card w-100" style="margin-bottom: 1.5em;" type="text" name="station">
+					</label>
+					<label>Stream URL
+						<input class="card w-100" style="margin-bottom: 1.5em;" type="text" name="url">
+					</label>
+					<label>Logo URL
+						<input class="card w-100" style="margin-bottom: 1.5em;" type="text" name="logo">
+					</label>
+					<div>
+						<label>Station to remove
+							<select class="card w-100" style="margin-bottom: 1.5em;" name="rmstation">
+								<option value="--" selected>Select station</option>
+								<?php
+								$files = glob($dir . DIRECTORY_SEPARATOR . "*");
+								foreach ($files as $file) {
+									echo "<option value='" . $file . "'>" . pathinfo($file)['filename'] . "</option>";
+								}
+								?>
+							</select>
+						</label>
+					</div>
+					<label>Password
+						<input class="card w-100" style="margin-bottom: 2em;" type="password" name="password">
+					</label>
+					<button class="btn primary" title="Add station" type="submit" name="add"><img style='vertical-align: middle;' src='svg/add.svg' /></button>
+					<button class="btn primary" title="Remove station" type="submit" name="remove"><img style='vertical-align: middle;' src='svg/remove.svg' /></button>
+				</form>
+				<button class="btn secondary" style="display: inline;" title="Back" onclick='window.location.href = "index.php"'><img style='vertical-align: middle;' src='svg/back.svg' /></button>
+				<?php
+				if (isset($_POST['add']) && ($_POST['password'] == $password)) {
+					file_put_contents($dir . DIRECTORY_SEPARATOR . $_POST['station'], $_POST['url'] . PHP_EOL . $_POST['logo']);
+					echo "<script>";
+					echo 'alert("' . $_POST['station'] . ' has been added.")';
+					echo "</script>";
+				}
+				if (isset($_POST['remove']) && ($_POST['password'] == $password)) {
+					unlink($_POST['rmstation']);
+					echo "<script>";
+					echo 'alert("' . pathinfo($_POST['rmstation'])['filename'] . ' has been removed.")';
+					echo "</script>";
+				}
+				?>
 			</div>
-			<label>Password:
-				<input type="password" name="password">
-			</label>
-			<div class="text-center">
-				<button title="Add station" type="submit" name="add"><img style='vertical-align: middle;' src='svg/add.svg' /></button>
-				<button title="Remove station" type="submit" name="remove"><img style='vertical-align: middle;' src='svg/remove.svg' /></button>
+			<div style="margin-bottom: 1em; margin-top: 1em;">
+				<?php echo $footer; ?>
 			</div>
-		</form>
-		<?php
-		if (isset($_POST['add']) && ($_POST['password'] == $password)) {
-			file_put_contents($dir . DIRECTORY_SEPARATOR . $_POST['station'], $_POST['url'] . PHP_EOL . $_POST['logo']);
-			echo "<script>";
-			echo 'alert("' . $_POST['station'] . ' has been added.")';
-			echo "</script>";
-		}
-		if (isset($_POST['remove']) && ($_POST['password'] == $password)) {
-			unlink($_POST['rmstation']);
-			echo "<script>";
-			echo 'alert("' . pathinfo($_POST['rmstation'])['filename'] . ' has been removed.")';
-			echo "</script>";
-		}
-		?>
-		<div style="margin-bottom: 1em; margin-top: 1em;">
-			<?php echo $footer; ?>
 		</div>
-	</div>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('config.php');
 <!DOCTYPE html>
 <html lang="en" data-theme="<?php echo $theme; ?>">
 
-<!-- Author: Dmitri Popov, dmpop@linux.com
+<!-- Author: Dmitri Popov, dmpop@cameracode.coffee
 	 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 
 <head>
@@ -13,8 +13,7 @@ include('config.php');
 	<meta charset="utf-8">
 	<link rel="shortcut icon" href="favicon.png" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/classless.css" />
-	<link rel="stylesheet" href="css/themes.css" />
+	<link rel="stylesheet" href="lit.css" />
 	<!-- Suppress form re-submit prompt on refresh -->
 	<script>
 		if (window.history.replaceState) {
@@ -24,50 +23,50 @@ include('config.php');
 </head>
 
 <body>
-	<div class="card text-center">
-		<div style="margin-top: 1em; margin-bottom: 1em;">
-			<img style="display: inline; height: 2.5em; vertical-align: middle;" src="favicon.svg" alt="logo" />
-			<h1 style="display: inline; margin-top: 0em; vertical-align: middle; letter-spacing: 3px; color: #ffcc02;"><?php echo $title; ?></h1>
-		</div>
-		<hr style="margin-bottom: 2em;">
-		<?php
-		if (!file_exists($dir)) {
-			mkdir($dir, 0750);
-			$url = "http://stream.antenne.de/80er-kulthits";
-			$logo = "http://www.antenne.de/assets/icons/antenne-de/apple-touch-icon.png?v=3";
-			file_put_contents($dir . DIRECTORY_SEPARATOR . "Radio station 1", $url . PHP_EOL . $logo);
-		}
-		?>
-		<form action=" " method="POST">
-			<select style="vertical-align: middle;" name="station">
-				<option value="--" selected>Select station</option>
+	<div class="c">
+		<div style="text-align: center;">
+			<div class="card w-100">
+				<img style="display: inline; height: 2.5em; vertical-align: middle;" src="favicon.svg" alt="logo" />
+				<h1 style="display: inline; margin-top: 0em; vertical-align: middle; letter-spacing: 3px; color: #ce6a85;"><?php echo $title; ?></h1>
 				<?php
-				$files = glob($dir . DIRECTORY_SEPARATOR . "*");
-				foreach ($files as $file) {
-					echo "<option value='" . $file . "'>" . pathinfo($file)['filename'] . "</option>";
+				if (!file_exists($dir)) {
+					mkdir($dir, 0750);
+					$url = "http://stream.antenne.de/80er-kulthits";
+					$logo = "http://www.antenne.de/assets/icons/antenne-de/apple-touch-icon.png?v=3";
+					file_put_contents($dir . DIRECTORY_SEPARATOR . "Radio station 1", $url . PHP_EOL . $logo);
 				}
 				?>
-			</select>
-			<button style="vertical-align: middle;" title="Stream the selected radio station" type='submit' name='play'><img style='vertical-align: middle;' src='svg/play.svg' /></button>
-			<button style="vertical-align: middle;" title="Stop streaming" type="submit" onClick="window.location.reload(true)"><img style='vertical-align: middle;' src='svg/stop.svg' /></button>
-		</form>
-		<?php
-		if (isset($_POST['play'])) {
-			$lines = file($_POST['station']);
-			if (isset($lines[1])) {
-				echo "<img style='margin-top: 1em; width:128px; max-width:100%; border-radius: 7px;' src='$lines[1]' />";
-			} else {
-				echo '<img style="margin-top: 1em;" src="audio.svg" />'; // Source: https://samherbert.net/svg-loaders/
-			}
-			echo "<h3 style='margin-top: 1em;'>" . pathinfo($_POST['station'])['filename'] . "</h3>";
-			echo '<audio controls autoplay style="width: 100%; margin-top: 1em;"> <source src="' . $lines[0] . '">Audio tag is not supported in this browser.</audio></p>';
-		}
-		?>
-		<button title="Edit station list" style="margin-top: 1.5em; margin-bottom: 1.5em;" onclick='window.location.href = "edit.php"'><img style='vertical-align: middle;' src='svg/edit.svg' /></button>
-		<div style="margin-bottom: 1em;">
-			<?php echo $footer; ?>
+				<form style="margin-top: 2em;" action=" " method="POST">
+					<select class="card w-50" style="vertical-align: middle;" name="station">
+						<option value="--" selected>Select station</option>
+						<?php
+						$files = glob($dir . DIRECTORY_SEPARATOR . "*");
+						foreach ($files as $file) {
+							echo "<option value='" . $file . "'>" . pathinfo($file)['filename'] . "</option>";
+						}
+						?>
+					</select>
+					<button class="btn primary" style="vertical-align: middle;" title="Stream the selected radio station" type='submit' name='play'><img style='vertical-align: middle;' src='svg/play.svg' /></button>
+					<button class="btn primary" style="vertical-align: middle;" title="Stop streaming" type="submit" onClick="window.location.reload(true)"><img style='vertical-align: middle;' src='svg/stop.svg' /></button>
+				</form>
+				<?php
+				if (isset($_POST['play'])) {
+					$lines = file($_POST['station']);
+					if (isset($lines[1])) {
+						echo "<img style='margin-top: 1em; width:128px; max-width:100%; border-radius: 7px;' src='$lines[1]' />";
+					} else {
+						echo '<img style="margin-top: 1em;" src="audio.svg" />'; // Source: https://samherbert.net/svg-loaders/
+					}
+					echo "<h3 style='margin-top: 1em;'>" . pathinfo($_POST['station'])['filename'] . "</h3>";
+					echo '<audio controls autoplay style="width: 100%; margin-top: 1em;"> <source src="' . $lines[0] . '">Audio tag is not supported in this browser.</audio></p>';
+				}
+				?>
+				<button class="btn primary" title="Edit station list" style="margin-top: 1.5em; margin-bottom: 1.5em;" onclick='window.location.href = "edit.php"'><img style='vertical-align: middle;' src='svg/edit.svg' /></button>
+			</div>
+			<div style="margin-bottom: 1em;">
+				<?php echo $footer; ?>
+			</div>
 		</div>
-	</div>
 </body>
 
 </html>
